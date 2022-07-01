15d90a7f47e42bb230481c1511c0eceb5bccc657
sihagwak <sasha.gwak@gmail.com>
2022-07-01 22:48:59 +0900

    [BOJ] 빠른 A+B(15552번)

--- BOJ/2207012213.py
+++ BOJ/2207012213.py
@@ -4,7 +4,7 @@ T = int(input())
 sum = []
 
 for i in range(0, T):
-    # 반복문으로 여러줄을 입력받아야 할 때 input 사용하면 시관초과 발생할 수있음! 
+    # 반복문으로 여러줄을 입력받아야 할 때 input 사용하면 시간초과 발생할 수있음! 
     a, b = map(int, sys.stdin.readline().split())
     sum.append(a + b)
 
