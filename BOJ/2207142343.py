ae6eb6523c62b232baab11adb98d82d0968da815
sihagwak <sasha.gwak@gmail.com>
2022-07-15 01:55:45 +0900

    [BOJ] 더하기싸이클 1110

--- BOJ/2207142343.py
+++ BOJ/2207142343.py
@@ -3,21 +3,18 @@
 # 26부터 시작한다. 2+6 = 8이다. 새로운 수는 68이다. 6+8 = 14이다. 새로운 수는 84이다. 8+4 = 12이다. 새로운 수는 42이다. 4+2 = 6이다. 새로운 수는 26이다.
 # 위의 예는 4번만에 원래 수로 돌아올 수 있다. 따라서 26의 사이클의 길이는 4이다.
 # N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
-N = input()
-get = int(N) 
-# 앞, 뒤의 숫자 저장
-f = 0
-b = 0
-# 사이클 숫자 저장 
-c = 0
+N = int(input())
+num = N
+count = 0
 
 while True:
-    for i in range(0,2):
-        if i == 1:
-            f = int(N[1])
-        b += int(N[i])
-        c += 1
-    N = f + b
-    if N == get:
-        print(c)
+    a = num // 10
+    b = num % 10
+    c = (a + b) % 10
+    num = (b * 10) + c
+    count += 1
+
+    if N == num:
         break
+
+print(count)
